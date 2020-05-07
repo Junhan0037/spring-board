cd191ffdb20a5ab1433fa07044e1e3a16167dfaa
Junhan0037 <wnsgks0037@naver.com>
2020-05-07 23:31:54 +0900

    Add posts-save

--- src/main/java/me/whiteship/springboot/web/IndexController.java
+++ src/main/java/me/whiteship/springboot/web/IndexController.java
@@ -1,14 +1,22 @@
 package me.whiteship.springboot.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+// 페이지 관련된 컨트롤러
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     @GetMapping("/")
     public String index() {
-        return "index"; // index.mustache
+        return "index"; // index.mustache 호출
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save"; // posts-save.mustache 호출
     }
 
 }
