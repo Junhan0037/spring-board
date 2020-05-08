33068a295c476f06a3d17efc7c565c3b17f8ef59
Junhan0037 <wnsgks0037@naver.com>
2020-05-09 00:07:15 +0900

    Add findAllDesc() method

--- src/main/java/me/whiteship/springboot/service/PostsService.java
+++ src/main/java/me/whiteship/springboot/service/PostsService.java
@@ -3,11 +3,15 @@ package me.whiteship.springboot.service;
 import lombok.RequiredArgsConstructor;
 import me.whiteship.springboot.domain.posts.Posts;
 import me.whiteship.springboot.domain.posts.PostsRepository;
+import me.whiteship.springboot.web.dto.PostsListResponseDto;
 import me.whiteship.springboot.web.dto.PostsResponseDto;
 import me.whiteship.springboot.web.dto.PostsSaveRequestDto;
 import me.whiteship.springboot.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,6 +35,11 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) // 트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회속도가 개선된다. (등록, 수정, 삭제 기능이 전혀없는 서비스 메소드에 추천)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 
 }
