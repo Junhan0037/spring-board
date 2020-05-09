43d1091060dfc4e6a65635b0bd03d002c79987cc
Junhan0037 <wnsgks0037@naver.com>
2020-05-09 13:34:03 +0900

    Add 연결 링크 추가

--- src/main/java/me/whiteship/springboot/web/IndexController.java
+++ src/main/java/me/whiteship/springboot/web/IndexController.java
@@ -1,22 +1,36 @@
 package me.whiteship.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import me.whiteship.springboot.service.PostsService;
+import me.whiteship.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-// 페이지 관련된 컨트롤러
+// 페이지 관련 컨트롤러
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model. addAttribute("posts", postsService.findAllDesc()); // index.mustache에 posts로 전달
         return "index"; // index.mustache 호출
     }
 
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save"; // posts-save.mustache 호출
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update"; // posts-update.mustache 호출
     }
 
 }
