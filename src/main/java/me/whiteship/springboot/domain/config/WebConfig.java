bd9555e36e59c6bf91b7211e5bd9456898b47465
Junhan0037 <wnsgks0037@naver.com>
2020-05-10 00:34:23 +0900

    스프링에 LoginUserArgumentResolver 인식

--- src/main/java/me/whiteship/springboot/domain/config/WebConfig.java
+++ src/main/java/me/whiteship/springboot/domain/config/WebConfig.java
@@ -1,4 +1,22 @@
 package me.whiteship.springboot.domain.config;
 
-public class WebConfig {
+import lombok.RequiredArgsConstructor;
+import me.whiteship.springboot.domain.config.auth.LoginUserArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer { // LoginUserArgumentResolver를 스프링에서 인식될 수 있도록 한다.
+
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver);
+    }
+
 }
