2f1cbe6eb23c3eae35a700e72ce5b1a2d0eafa0d
Junhan0037 <wnsgks0037@naver.com>
2020-05-09 17:07:43 +0900

    Add 구글 로그인 후 조치

---
+++ src/main/java/me/whiteship/springboot/domain/config/auth/CustomOAuth2UserService.java
@@ -0,0 +1,55 @@
+package me.whiteship.springboot.domain.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import me.whiteship.springboot.domain.config.auth.dto.OAuthAttributes;
+import me.whiteship.springboot.domain.config.auth.dto.SessionUser;
+import me.whiteship.springboot.domain.user.User;
+import me.whiteship.springboot.domain.user.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
+
+// 구글 로그인 이후 가져온 사용자의 정보들을 기반으로 기능들을 지원하는 서비스 클래스.
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    private final UserRepository userRepository;
+    private final HttpSession httpSession;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2UserService delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId(); // 현재 로그인 진행 중인 서비스를 구분 (구글, 네이버 등)
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName(); // OAuth2 로그인 진행 시 키가 되는 필드값을 의미.
+
+        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes()); // Oauther2User의 attribute를 담을 클래스.
+
+        User user = saveOrUpdate(attributes);
+        httpSession.setAttribute("user", new SessionUser(user)); // 세션에 사용자 정보를 저장하기 위한 Dto클래스.
+
+        return new DefaultOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
+                attributes.getAttributes(),
+                attributes.getNameAttributeKey());
+    }
+
+    private User saveOrUpdate(OAuthAttributes attributes) {
+        User user = userRepository.findByEmail(attributes.getEmail())
+                .map(entity -> entity.update(attributes.getName(), attributes.getPicture())) // 기존 로그인한 경험이 있는 경우
+                .orElse(attributes.toEntity()); // 처음 가입할 경우
+
+        return userRepository.save(user);
+    }
+
+}
