adea26ac7b8258f136796141821dda30f9eb4dbe
Junhan0037 <wnsgks0037@naver.com>
2020-05-10 00:34:56 +0900

    Add comment

--- src/main/java/me/whiteship/springboot/domain/config/auth/LoginUserArgumentResolver.java
+++ src/main/java/me/whiteship/springboot/domain/config/auth/LoginUserArgumentResolver.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Component
-public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver { // 조건에 맞는 경우 메소드가 있다면 구현체가 지정한 값으로 해당 메소드의 파라미터로 넘길 수 있다.
 
     private final HttpSession httpSession;
 
